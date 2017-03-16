ec681488027169b7d5c630a8fb58bcd01293b364
Isaias Fernandes <isaiasmelof@Mac320.local>
2017-03-16 18:02:55 -0300

    Initial Commit

---
+++ ChallengeOO/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ChallengeOO
+//
+//  Created by Isaias Fernandes on 16/03/17.
+//  Copyright © 2017 Isaias Fernandes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
